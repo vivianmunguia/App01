83bbc6fce38d059b7d15a0efc51827f4df9473c9
vivianmunguia <andy_munguia@hotmail.com>
2024-01-04 19:28:47 -0700

    The project has been created and string variables were printed

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Este comentario es de una sola linea
+/*
+ Comentario de varias líneas
+ */
+
+string nombre = "Vivian Munguía";
+var url = "www.vivianmunguia.com";
+
+Console.WriteLine("Hola alumnos del curso de C# " + nombre + " url: " + url);
